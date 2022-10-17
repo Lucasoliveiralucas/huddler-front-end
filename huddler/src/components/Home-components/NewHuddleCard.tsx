0c4be6581d0bdd43991e595781b24eb2d7a74121
Lucas de Oliveira Santos <catadorfgh@outlook.com>
2022-10-17 11:01:20 +0200

    feat: testing chat

--- huddler/src/components/Home-components/NewHuddleCard.tsx
+++ huddler/src/components/Home-components/NewHuddleCard.tsx
@@ -9,6 +9,7 @@ import {
   removeUserGoingToHuddle,
 } from "../../utils/APIServices/huddleServices";
 import { useAuth } from "../../contexts/AuthContext";
+import Link from "next/link";
 
 type Props = {
   huddle: Huddle;
@@ -49,93 +50,95 @@ function NewHuddleCard({
   }, []);
 
   return (
-    <div className="ml-3 mr-3 mt-3">
-      <div className="flex mb-1">
-        <h1 className="font-extrabold text-palette-orange text-2xl">
-          {huddle.name}
-        </h1>
-        <div className="ml-auto mr-3">
-          {going ? (
-            <button
-              className="justify-center w-14 bg-palette-orange bg-opacity-40 text-lg border-solid border-[0.5px] border-palette-orange shadow-md rounded-lg hover:bg-opacity-60"
-              onClick={(e) => {
-                setGoing(!going);
-                setUpdate(!update);
-                removeUserGoingToHuddle(currentUser, huddle.id);
-              }}
-            >
-              Leave
-            </button>
-          ) : (
-            <button
-              className="justify-center w-14 bg-palette-orange bg-opacity-40 text-lg border-solid border-[0.5px] border-palette-orange shadow-md rounded-lg hover:bg-opacity-60"
-              onClick={(e) => {
-                setGoing(!going);
-                setUpdate(!update);
-                postUserGoingToHuddle(currentUser, huddle.id);
-              }}
-            >
-              Join
-            </button>
-          )}
+    <Link href={{ pathname: `/details/${huddle.id}`, query: huddle }}>
+      <div className="ml-3 mr-3 mt-3">
+        <div className="flex mb-1">
+          <h1 className="font-extrabold text-palette-orange text-2xl">
+            {huddle.name}
+          </h1>
+          <div className="ml-auto mr-3">
+            {going ? (
+              <button
+                className="justify-center w-14 bg-palette-orange bg-opacity-40 text-lg border-solid border-[0.5px] border-palette-orange shadow-md rounded-lg hover:bg-opacity-60"
+                onClick={(e) => {
+                  setGoing(!going);
+                  setUpdate(!update);
+                  removeUserGoingToHuddle(currentUser, huddle.id);
+                }}
+              >
+                Leave
+              </button>
+            ) : (
+              <button
+                className="justify-center w-14 bg-palette-orange bg-opacity-40 text-lg border-solid border-[0.5px] border-palette-orange shadow-md rounded-lg hover:bg-opacity-60"
+                onClick={(e) => {
+                  setGoing(!going);
+                  setUpdate(!update);
+                  postUserGoingToHuddle(currentUser, huddle.id);
+                }}
+              >
+                Join
+              </button>
+            )}
+          </div>
         </div>
-      </div>
-      <div className="grid grid-cols-2 md:flex h-full">
-        <div className="flex">
-          <div className="h-3/4 md:w-[24rem] mr-3">
-            <div className="rounded-lg h-32 lg:h-40 md:w-3/4 relative">
-              <Image
-                src={huddle.image}
-                fill
-                className="rounded-lg"
-                alt={huddle.name}
-              />
-            </div>
+        <div className="grid grid-cols-2 md:flex h-full">
+          <div className="flex">
+            <div className="h-3/4 md:w-[24rem] mr-3">
+              <div className="rounded-lg h-32 lg:h-40 md:w-3/4 relative">
+                <Image
+                  src={huddle.image}
+                  fill
+                  className="rounded-lg"
+                  alt={huddle.name}
+                />
+              </div>
 
-            <p>attending: {data.attending}</p>
+              <p>attending: {data.attending}</p>
 
-            <div className="hidden md:grid grid-cols-2 gap-2">
-              {data.categories.map((category, i) => {
-                return (
-                  i <= 3 && (
+              <div className="hidden md:grid grid-cols-2 gap-2">
+                {data.categories.map((category, i) => {
+                  return (
+                    i <= 3 && (
+                      <p
+                        className="text-center py-0.5 bg-palette-dark rounded-md text-white"
+                        key={category.id + (i - i)}
+                      >
+                        {category.name}
+                      </p>
+                    )
+                  );
+                })}
+              </div>
+              {/* Mobile */}
+              <div className="grid md:hidden grid-cols-2 gap-2 py-1">
+                {data.categories.map((category, i) => {
+                  return i > 1 ? (
+                    <></>
+                  ) : (
                     <p
-                      className="text-center py-0.5 bg-palette-dark rounded-md text-white"
-                      key={category.id + (i - i)}
+                      className="text-center py-1 bg-palette-dark rounded-md text-white"
+                      key={category.id}
                     >
                       {category.name}
                     </p>
-                  )
-                );
-              })}
+                  );
+                })}
+              </div>
             </div>
-            {/* Mobile */}
-            <div className="grid md:hidden grid-cols-2 gap-2 py-1">
-              {data.categories.map((category, i) => {
-                return i > 1 ? (
-                  <></>
-                ) : (
-                  <p
-                    className="text-center py-1 bg-palette-dark rounded-md text-white"
-                    key={category.id}
-                  >
-                    {category.name}
-                  </p>
-                );
-              })}
+            {/* Description */}
+            <div className="grid max-w-[300px] md:h-56 py-2 w-full space-x-0 ">
+              <p>{huddle.description}</p>
+              <p className="text-sm self-end">
+                At {huddle.address}
+                <br></br>
+                {dateTime.monthDayYear} at {dateTime.time}
+              </p>
             </div>
-          </div>
-          {/* Description */}
-          <div className="grid max-w-[300px] md:h-56 py-2 w-full space-x-0 ">
-            <p>{huddle.description}</p>
-            <p className="text-sm self-end">
-              At {huddle.address}
-              <br></br>
-              {dateTime.monthDayYear} at {dateTime.time}
-            </p>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
