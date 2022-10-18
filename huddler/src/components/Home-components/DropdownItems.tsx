6b9fc3b6d2d9d8034ebc258d4956b2bb6b3a0004
Apolo Paralelo <Donghoon@Donghoons-MBP.lan>
2022-10-18 18:06:23 +0200

    feat: dropdown menu filter at homepage

---
+++ huddler/src/components/Home-components/DropdownItems.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import { getHuddlesInCategory } from '../../utils/APIServices/categoryServices'
+import { Huddle, Category } from '../../types';
+
+type DropdownProps = {
+  categories: Category[];
+  showDropdown: boolean;
+  toggleDropdown: Function;
+  categorySelection: Function;
+  setFilterChoice: React.Dispatch<React.SetStateAction<Huddle[]>>
+}
+
+const DropdownItems: React.FC<DropdownProps> = ({ categories, categorySelection, setFilterChoice }:DropdownProps): JSX.Element => {
+  
+  const [showDropdown, setShowDropdwon] = useState<boolean>(false);
+
+  
+  const changeDisplayedCategory = async (category: Category) => {
+    const data: Huddle[] = await getHuddlesInCategory(category.id);
+    return data
+  };
+
+
+  const onClickHandler = async (category: Category) => {
+    categorySelection(category.name);
+    const HuddlesByCategory = await changeDisplayedCategory(category)
+    setFilterChoice(HuddlesByCategory);
+  }
+
+  useEffect(() => {
+    setShowDropdwon(showDropdown);
+  }, [showDropdown]);
+
+  return (
+    <div className={showDropdown ? 'dropdown' : 'dropdown active'}>
+      {categories.map(
+        (category: Category, index: number): JSX.Element => {
+          return (
+            <p
+              key={index}
+              onClick={(): void => {
+              onClickHandler(category);
+              }}
+              >
+                {category.name}
+              </p>
+            )
+          }
+        )}
+    </div>
+  )
+}
+
+export default DropdownItems;
