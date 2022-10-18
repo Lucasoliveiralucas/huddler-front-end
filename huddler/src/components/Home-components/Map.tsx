8a3fcc042a1946c648ec6064a8d3caa8f3b0757d
Fabian Lopez Gonzalez <fabianlopez@pm.me>
2022-10-18 11:14:24 +0200

    fix: readded the posting categories in new user form main form

--- huddler/src/components/Home-components/Map.tsx
+++ huddler/src/components/Home-components/Map.tsx
@@ -31,7 +31,6 @@ export default function Map({
   setLocation,
   updateList,
   user,
-  setLocation
 }: Props) {
   // const { currentUser } = useAuth();
   // const [user, setUser] = useState<User>();
