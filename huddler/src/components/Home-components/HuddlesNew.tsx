026449eb2f0d09d329b535c657b1335536c12015
ov <virtaoskari@gmail.com>
2022-10-18 11:33:06 +0200

    style(home): responsiveness

--- huddler/src/components/Home-components/HuddlesNew.tsx
+++ huddler/src/components/Home-components/HuddlesNew.tsx
@@ -19,11 +19,11 @@ type Props = {
 function Huddles({ huddles, updateList, id, huddlesUserIsGoing }: Props) {
 
     return (
-        <div className="flex flex-col justify-start px-2 h-full" id="carousel" >
-            <div className="gap-4 grid 2xl:grid-cols-2">
+        <div className="flex flex-col justify-start h-full" id="carousel" >
+            <div className="gap-3 grid 2xl:grid-cols-2">
             {huddles.map((huddle) => (
                 <div
-                    className="h-64 lg:h-72 p-2 flex-grow-1 3xl:pl-8 flex-shrink-0 shadow-md border-palette-dark hover:border-palette-orange bg-white bg-opacity-50 border relative rounded-lg"
+                    className="h-72 lg:h-80 p-2 flex-grow-1 3xl:pl-8 flex-shrink-0 shadow-md border-palette-dark hover:border-palette-orange bg-white bg-opacity-50 border relative rounded-lg"
                     key={huddle.id}
                 >
                     {/* <HuddleCarouselItem */}
