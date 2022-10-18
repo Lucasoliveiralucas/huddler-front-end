e20dc9499be0de8438aaaa0d998e27d06e2a3b2a
aferrarifirmo <anaferrarifirmo@gmail.com>
2022-10-18 09:13:45 +0200

    Landing page

--- huddler/pages/index.tsx
+++ huddler/pages/index.tsx
@@ -17,17 +17,26 @@ const LandingPage: NextPage = () => {
   ]
 
   return (
-    // <><Slider></Slider>
-    <div className="flex w-full h-full justify-center relative">
-      {/* Slider component here */}
-      <Image alt={"image"} src={background[2]} sizes="100%" priority={true} />
-      {/* <Slider></Slider> */}
-      <div>
-        <button className="cursor-pointer">Log in</button>
-      </div>
-      <Register />
+    <div className="w-full h-screen justify-center relative">
+      <Slider></Slider>
+      <button className="cursor-pointer
+                       text-white
+                       bg-palette-orange
+                       absolute
+                       transform
+                       top-10
+                       right-10
+                       px-6
+                       p-2
+                       z-10
+                       rounded-3xl
+                       hover:scale-105
+                       " >Log in</button>
+      {/* <Image alt={"image"} src={background[2]} sizes="100%" priority={true} /> */}
+      <h1 className="absolute z-10 left-[40vw] top-28 text-6xl font-extrabold text-slate-50">HUDDLER</h1>
+      {/* <Register /> */}
     </div>
-    // </>
+    
   );
 };
 export default LandingPage;
--- huddler/src/components/Slider.tsx
+++ huddler/src/components/Slider.tsx
@@ -9,26 +9,33 @@ import Image from "next/future/image";
 // import nn from '../../public/bg_images/Bike_img'
 
 const fadeImages = [
-  // {url: "https://cdn.https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/roa070120fea-bikesupcoast-03-1595952354.jpgmotor1.com/images/mgl/JR4VM/s3/12-reasons-to-ride-a-motorcycle.jpg"},
-  {url: "https://www.thespruce.com/thmb/W7fMK4ljIYbKtW9YdiFOyozjhfU=/1600x1200/smart/filters:no_upscale()/GettyParty-5b35eb3c46e0fb003788537e.jpg"},
+  {url: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/roa070120fea-bikesupcoast-03-1595952354.jpg"},
   {url: "https://static.toiimg.com/photo/msid-75490045,width-96,height-65.cms"},
+  // {url: "https://blog.ruoff.com/hubfs/DIY-Outdoor-Lighting-Projects-Light-Up-Your-Next-Backyard-Gathering.jpg"},
   {url: "https://cdn.shopify.com/s/files/1/0021/5701/9254/articles/Felt_Bicycles_Group_Ride_Tips_MAIN_2048x.jpg"}];
 
 const Slider = () => {
 
   return (
-    <div className="slide-container">
-      <Slide>
-        {fadeImages.map((fadeImage, key) => (
-          <div className="each-slide" key={key}> 
-            <div style={{backgroundImage: `url(${fadeImage.url})`}}>
-       
-              {/* <img width="800" height="600" alt="background-slider" src={fadeImage.url} /> */}
-            </div>
+    <>
+    <div className="slide-container w-screen bg-black">
+      <Fade>
+        {fadeImages.map((fadeImage, i) => (
+          <>
+          <div className="absolute w-screen h-screen bg-[#1a0d05] opacity-75" key={i}></div>
+          <div className="each-fade brightness-[0.25]" key={i}
+          style={
+            {backgroundImage: `url(${fadeImage.url})`,
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            }}> 
           </div>
+          </>
         ))}
-      </Slide>
+      </Fade>
     </div>
+    </>
   )
 
   
