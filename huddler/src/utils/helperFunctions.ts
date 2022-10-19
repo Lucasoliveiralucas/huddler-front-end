f7da7a0fca0f33b57fbd7d7385d75ff28976f3dd
Fabian Lopez Gonzalez <fabianlopez@pm.me>
2022-10-19 18:07:30 +0200

    feat: recommended not showing own created huddles working

--- huddler/src/utils/helperFunctions.ts
+++ huddler/src/utils/helperFunctions.ts
@@ -2,7 +2,10 @@ import { Auth } from 'aws-amplify';
 import dayjs from 'dayjs';
 import { Category, Huddle } from '../types';
 import { getHuddlesInCategory } from './APIServices/categoryServices';
-import { getUserCategories, getUserCreatedHuddles } from './APIServices/userServices';
+import {
+  getUserCategories,
+  getUserCreatedHuddles,
+} from './APIServices/userServices';
 
 // 1. Fetcher
 // 2. Recommended Huddles
@@ -16,7 +19,7 @@ export const fetcher = async (...args: string[]) => {
     const data = await fetch(...args);
     return await data.json();
   } catch (e) {
-    console.log("There has been an error fetching data: ", e);
+    console.log('There has been an error fetching data: ', e);
     return e;
   }
 };
@@ -24,35 +27,33 @@ export const fetcher = async (...args: string[]) => {
 // 2. Recommended Huddles
 
 //For now this functions returns all the huddles that are in user categories. Eventually we should do some kind of sorting or also recommend by location. Now we don't have enough huddles in each categories to test it.
+
 export const recommendedForUser = async (aws_id: string) => {
   const userCategories = await getUserCategories(aws_id);
   const promises = userCategories.map((category: Category) =>
     getHuddlesInCategory(category.id as number)
   );
   const huddlesInCategories = await Promise.all(promises);
-  console.log('huddles in categories', huddlesInCategories)
-  // const huddlesInCategoriesArr = huddlesInCategories.reduce(
-  //   (previousValue, currentValue) => [...previousValue, ...currentValue],
-  //   []
-  // );
-  const userCreated = await getUserCreatedHuddles(aws_id)
-  console.log('userCreated', userCreated)
+  const huddlesInCategoriesArr = huddlesInCategories.reduce(
+    (previousValue, currentValue) => [...previousValue, ...currentValue],
+    []
+  );
+  console.log('huddles in categories', huddlesInCategoriesArr);
+  const userCreated = await getUserCreatedHuddles(aws_id);
+
+  console.log('userCreated', userCreated);
+
   //To not recommend the user's created huddles
   const recommendNotCreated: Huddle[] = [];
-  huddlesInCategories.forEach((huddle: Huddle) => {
-    console.log('huddleName0', huddle.name)
-    if (userCreated!.some((hud: Huddle) => hud.name !== huddle.name)) {
-      recommendNotCreated.push(huddle)}
-  })
 
-    // userCreated.forEach((huddle: Huddle) => {
-    //   if (
-    //     !huddlesInCategories!.some((hud: Huddle) => hud.name === huddle.name)
-    //   )
+  huddlesInCategoriesArr.forEach((huddle: Huddle) => {
+  
+    if (!userCreated!.some((hud: Huddle) => hud.name === huddle.name)) {
+      recommendNotCreated.push(huddle);
+    }
+  });
 
-    //     recommendNotCreated.push(huddle);
-    // });
-    console.log('recommendNotCreated', recommendNotCreated)
+  // console.log('recommendNotCreated', recommendNotCreated)
   return recommendNotCreated;
 };
 
@@ -60,17 +61,17 @@ export const recommendedForUser = async (aws_id: string) => {
 export const dateFormatter = (date: string) => {
   const toFormat = dayjs(date);
   const dateTime = {
-    day: toFormat.format("DD"),
-    month: toFormat.format("MMMM"),
-    year: toFormat.format("YYYY"),
-    time: toFormat.format("HH:mm"),
-    monthDayYear: toFormat.format("MMMM DD, YYYY"),
+    day: toFormat.format('DD'),
+    month: toFormat.format('MMMM'),
+    year: toFormat.format('YYYY'),
+    time: toFormat.format('HH:mm'),
+    monthDayYear: toFormat.format('MMMM DD, YYYY'),
   };
   return dateTime;
 };
 
 export const nowFormatted = () => {
-  return dayjs(Date.now()).format("YYYY-MM-DDTHH:mm");
+  return dayjs(Date.now()).format('YYYY-MM-DDTHH:mm');
 };
 
 // 4. Sort by name
@@ -96,3 +97,4 @@ export const getActiveHuddles = (huddlesToFileter: Huddle[]) => {
     (huddle) => new Date(huddle.day_time) > Date.now()
   );
 };
+
