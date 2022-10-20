aa6a2d99edce1bdfc4f728a4a484e468cadd41cb
Lucas de Oliveira Santos <catadorfgh@outlook.com>
2022-10-20 18:11:41 +0200

    feat: possibly broken

--- huddler/src/components/Slider.tsx
+++ huddler/src/components/Slider.tsx
@@ -8,7 +8,9 @@ const fadeImages = [
   {
     url: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/roa070120fea-bikesupcoast-03-1595952354.jpg",
   },
-
+  {
+    url: "https://static.toiimg.com/photo/msid-75490045,width-96,height-65.cms",
+  },
   {
     url: "https://cdn.shopify.com/s/files/1/0021/5701/9254/articles/Felt_Bicycles_Group_Ride_Tips_MAIN_2048x.jpg",
   },
@@ -32,22 +34,21 @@ const Slider = () => {
               fill
             />
           </div>
-
-          <Fade>
-            {fadeImages.map((fadeImage, key) => (
-              <div
-                className="each-fade brightness-50"
-                key={key}
-                style={{
-                  backgroundImage: `url(${fadeImage.url})`,
-                  backgroundPosition: "center",
-                  backgroundRepeat: "no-repeat",
-                  backgroundSize: "cover",
-                }}
-              ></div>
-            ))}
-          </Fade>
         </div>
+        <Fade>
+          {fadeImages.map((fadeImage, key) => (
+            <div
+              className="each-fade brightness-[0.25]"
+              key={key}
+              style={{
+                backgroundImage: `url(${fadeImage.url})`,
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover",
+              }}
+            ></div>
+          ))}
+        </Fade>
       </div>
     </>
   );
