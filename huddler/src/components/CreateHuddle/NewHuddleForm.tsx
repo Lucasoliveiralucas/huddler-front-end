1b7c041f06b51f9c0be25d18d0c195a8d2466d8f
Apolo Paralelo <Donghoon@Donghoons-MBP.lan>
2022-10-20 11:52:32 +0200

    displayImgCreateHuddle

--- huddler/src/components/CreateHuddle/NewHuddleForm.tsx
+++ huddler/src/components/CreateHuddle/NewHuddleForm.tsx
@@ -354,11 +354,10 @@ const NewHuddleForm = ({ data, setCenter, center, id }: Props) => {
               />
             </div>
             {imageSelected && (
-              <figure>
+              <figure className="flex justify-center items-center">
                 <Image
-                  className='ml-10'
-                  width={100}
-                  height={100}
+                  width={200}
+                  height={200}
                   id='image-preview'
                   alt='image-preview'
                   src={imagePreview}
