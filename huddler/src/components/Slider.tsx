537a2e89468cafcc85305066a2e4af2b2a6d4abc
Lucas de Oliveira Santos <catadorfgh@outlook.com>
2022-10-20 14:41:53 +0200

    fix: fixed image

--- huddler/src/components/Slider.tsx
+++ huddler/src/components/Slider.tsx
@@ -8,9 +8,7 @@ const fadeImages = [
   {
     url: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/roa070120fea-bikesupcoast-03-1595952354.jpg",
   },
-  {
-    url: "https://static.toiimg.com/photo/msid-75490045,width-96,height-65.cms",
-  },
+
   {
     url: "https://cdn.shopify.com/s/files/1/0021/5701/9254/articles/Felt_Bicycles_Group_Ride_Tips_MAIN_2048x.jpg",
   },
@@ -19,33 +17,37 @@ const fadeImages = [
 const Slider = () => {
   return (
     <>
-      <div className="slide-container w-full relative bg-black">
-        <div className="absolute flex w-full justify-center top-[12%] md:top-[8%] lg:top-[5%]">
-          <Image
-            alt="logo"
-            src={logo}
-            placeholder="blur"
-            width={800}
-            height={150}
-            objectFit="contain"
-            className="z-10"
+      <div className="slide-container w-screen bg-black relative">
+        <div className="absolute left-[37.5vw] top-[10%]">
+          <div className="relative h-[25rem] w-[30rem]">
+            <Image
+              alt="logo"
+              src={logo}
+              placeholder="blur"
+              quality={100}
+              sizes="50%"
+              // @ts-ignore
+              objectFit="contain"
+              className="z-10"
+              fill
             />
+          </div>
+
+          <Fade>
+            {fadeImages.map((fadeImage, key) => (
+              <div
+                className="each-fade brightness-50"
+                key={key}
+                style={{
+                  backgroundImage: `url(${fadeImage.url})`,
+                  backgroundPosition: "center",
+                  backgroundRepeat: "no-repeat",
+                  backgroundSize: "cover",
+                }}
+              ></div>
+            ))}
+          </Fade>
         </div>
-        
-        <Fade>
-          {fadeImages.map((fadeImage, key) => (
-            <div
-              className="each-fade brightness-50"
-              key={key}
-              style={{
-                backgroundImage: `url(${fadeImage.url})`,
-                backgroundPosition: "center",
-                backgroundRepeat: "no-repeat",
-                backgroundSize: "cover",
-              }}
-            ></div>
-          ))}
-        </Fade>
       </div>
     </>
   );
