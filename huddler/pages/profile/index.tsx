4d3047cd1229b355df01e9878a7b3ecff623b84c
Lucas de Oliveira Santos <catadorfgh@outlook.com>
2022-10-24 17:59:21 +0200

    fix: merge fix

--- huddler/pages/profile/index.tsx
+++ huddler/pages/profile/index.tsx
@@ -1,32 +1,32 @@
-import React, { useEffect, useRef, useState } from 'react';
-import Avatar from '../../src/components/Profile components/Avatar';
-import UserInfo from '../../src/components/Profile components/UserSidebar';
-import useSWR from 'swr';
-import HuddleCarousel from '../../src/components/Profile components/HuddleCarousel';
+import React, { useEffect, useRef, useState } from "react";
+import Avatar from "../../src/components/Profile components/Avatar";
+import UserInfo from "../../src/components/Profile components/UserSidebar";
+import useSWR from "swr";
+import HuddleCarousel from "../../src/components/Profile components/HuddleCarousel";
 import {
   fetcher,
   recommendedByInterests,
   userGoingNotCreated,
-} from '../../src/utils/helperFunctions';
-import { Category, Huddle, User } from '../../src/types';
-import MobileAvatar from '../../src/components/Profile components/MobileAvatar';
+} from "../../src/utils/helperFunctions";
+import { Category, Huddle, User } from "../../src/types";
+import MobileAvatar from "../../src/components/Profile components/MobileAvatar";
 import {
   getUserById,
   getUserCreatedHuddles,
   getUserGoingHuddles,
-} from '../../src/utils/APIServices/userServices';
-import { getHuddlesInCategory } from '../../src/utils/APIServices/categoryServices';
-import HuddleCarouselItem from '../../src/components/Profile components/HuddleCarouselItem';
-import { withSSRContext } from 'aws-amplify';
-import { NextApiResponse } from 'next/types';
-import { NextApiRequest } from 'next/types';
+} from "../../src/utils/APIServices/userServices";
+import { getHuddlesInCategory } from "../../src/utils/APIServices/categoryServices";
+import HuddleCarouselItem from "../../src/components/Profile components/HuddleCarouselItem";
+import { withSSRContext } from "aws-amplify";
+import { NextApiResponse } from "next/types";
+import { NextApiRequest } from "next/types";
 import {
   sortHuddlesByDate,
   getActiveHuddles,
-} from '../../src/utils/helperFunctions';
-import { GrDuplicate } from 'react-icons/gr';
-import Link from 'next/link';
-import { getAllHuddles } from '../../src/utils/APIServices/huddleServices';
+} from "../../src/utils/helperFunctions";
+import { GrDuplicate } from "react-icons/gr";
+import Link from "next/link";
+import { getAllHuddles } from "../../src/utils/APIServices/huddleServices";
 
 type Props = {
   aws_id: string;
@@ -43,7 +43,7 @@ function Profile({ aws_id, user, goingTo, recommended, huddles }: Props) {
   const [userCreatedHuddlesActive, setUserCreatedHuddlesActive] =
     useState<Huddle[]>();
   const [lastRow, setLastRow] = useState({
-    name: 'Recommended',
+    name: "Recommended",
     huddles: recommended,
   });
   //Get user id from auth for the tag hook
@@ -63,7 +63,7 @@ function Profile({ aws_id, user, goingTo, recommended, huddles }: Props) {
       );
       setHuddlesUserIsGoing(finalGoing);
     } catch (err) {
-      console.log('Error sorting or filtering huddles', err);
+      console.log("Error sorting or filtering huddles", err);
     }
   };
 
@@ -74,21 +74,21 @@ function Profile({ aws_id, user, goingTo, recommended, huddles }: Props) {
   const changeDisplayedCategory = async (category: Category) => {
     const data = await getHuddlesInCategory(category.id);
     if (lastRow.name == category.name) {
-      document.getElementById(category.name)?.classList.remove('bg-orange-600');
+      document.getElementById(category.name)?.classList.remove("bg-orange-600");
       document
         .getElementById(category.name)
-        ?.classList.remove('text-palette-light');
-      document.getElementById(category.name)?.classList.remove('border-none');
+        ?.classList.remove("text-palette-light");
+      document.getElementById(category.name)?.classList.remove("border-none");
       setLastRow({
-        name: 'Recommended',
+        name: "Recommended",
         huddles: recommended,
       });
     } else {
-      document.getElementById(category.name)?.classList.add('bg-orange-600');
+      document.getElementById(category.name)?.classList.add("bg-orange-600");
       document
         .getElementById(category.name)
-        ?.classList.add('text-palette-light');
-      document.getElementById(category.name)?.classList.add('border-none');
+        ?.classList.add("text-palette-light");
+      document.getElementById(category.name)?.classList.add("border-none");
       setLastRow({ name: category.name, huddles: data });
     }
   };
@@ -97,11 +97,11 @@ function Profile({ aws_id, user, goingTo, recommended, huddles }: Props) {
   if (!tags) return <div>loading...</div>;
 
   return (
-    <main className='flex flex-col mt-20 lg:mt-8 lg:grid lg:grid-cols-3 xl:grid-cols-4 h-full py-8 lg:bg-palette-light max-w-[99vw] gap-1'>
-      <div className='hidden lg:flex flex-col relative h-full max-h-[90vh]'>
+    <main className="flex flex-col mt-20 lg:mt-8 lg:grid lg:grid-cols-3 xl:grid-cols-4 h-full py-8 lg:bg-palette-light max-w-[99vw] gap-1">
+      <div className="hidden lg:flex flex-col relative h-full max-h-[90vh]">
         <div
-          className='flex flex-col h-full items-center
-          '
+          className="flex flex-col h-full items-center
+          "
         >
           <Avatar user={user} />
           <UserInfo
@@ -112,13 +112,13 @@ function Profile({ aws_id, user, goingTo, recommended, huddles }: Props) {
               huddlesUserIsGoing ? huddlesUserIsGoing.length : 0
             }
           />
-          <div className='h-1/9 w-full flex flex-col justify-center mt-8 border px-3'>
-            <h1 className='text-3xl font-karla text-palette-dark self-center mt-4 lg:mt-0'>
+          <div className="h-1/9 w-full flex flex-col justify-center mt-8 border px-3">
+            <h1 className="text-3xl font-karla text-palette-dark self-center mt-4 lg:mt-0">
               UPCOMING HUDDLE
             </h1>
-            <div className='mt-2 border-palette-dark hover:border-palette-orange bg-white bg-opacity-50 border rounded-lg flex justify-center'>
+            <div className="mt-2 border-palette-dark hover:border-palette-orange bg-white bg-opacity-50 border rounded-lg flex justify-center">
               {huddlesUserIsGoing && huddlesUserIsGoing.length && (
-                <div className='flex flex-col'>
+                <div className="flex flex-col">
                   <p>{huddlesUserIsGoing[0].name}</p>
                   <p>{huddlesUserIsGoing[0].day_time}</p>
                   <Link
@@ -128,7 +128,7 @@ function Profile({ aws_id, user, goingTo, recommended, huddles }: Props) {
                       query: huddlesUserIsGoing[0],
                     }}
                   >
-                    <h1 className='font-bold text-palette-dark text-lg cursor-pointer hover:text-palette-orange hover:underline'>
+                    <h1 className="font-bold text-palette-dark text-lg cursor-pointer hover:text-palette-orange hover:underline">
                       Event Page
                     </h1>
                   </Link>
@@ -140,7 +140,7 @@ function Profile({ aws_id, user, goingTo, recommended, huddles }: Props) {
       </div>
 
       {/* Mobile */}
-      <div className=' lg:hidden w-full h-auto flex-col'>
+      <div className=" lg:hidden w-full h-auto flex-col">
         <MobileAvatar user={user} />
         <UserInfo
           numOfCreatedHuddles={
@@ -152,17 +152,17 @@ function Profile({ aws_id, user, goingTo, recommended, huddles }: Props) {
         />
       </div>
 
-      <div className='h-full w-full col-span-2 xl:col-span-3 overflow-auto ml-0'>
-        <h1 className='pt-8 px-4 text-3xl font-yantra  text-palette-dark font-medium md:pl-10 lg:pl-0'>
+      <div className="h-full w-full col-span-2 xl:col-span-3 overflow-auto ml-0">
+        <h1 className="pt-8 px-4 text-3xl font-yantra  text-palette-dark font-medium md:pl-10 lg:pl-0">
           Interests:
         </h1>
         {Array.isArray(tags) && (
-          <div className='flex flex-wrap gap-4 p-4 md:pl-10 lg:pl-0'>
+          <div className="flex flex-wrap gap-4 p-4 md:pl-10 lg:pl-0">
             {tags.map((tag: Category) => (
               <h1
                 id={tag.name}
                 onClick={(e) => changeDisplayedCategory(tag)}
-                className='p-4
+                className="p-4
   text-center
   font-bold
   py-0.5
@@ -174,7 +174,7 @@ function Profile({ aws_id, user, goingTo, recommended, huddles }: Props) {
   cursor-pointer
   active:translate-x-[1px]
   active:translate-y-[1px]
-  hover:opacity-50'
+  hover:opacity-50"
                 key={tag.name}
               >
                 {tag.name}
@@ -186,7 +186,7 @@ function Profile({ aws_id, user, goingTo, recommended, huddles }: Props) {
         {Array.isArray(userCreatedHuddlesActive) &&
         userCreatedHuddlesActive.length ? (
           <>
-            <h1 className='pt-2 text-3xl font-bold font-yantra text-palette-dark'>
+            <h1 className="pt-2 text-3xl font-bold font-yantra text-palette-dark">
               Created huddles:
             </h1>
             {huddlesUserIsGoing && (
@@ -205,7 +205,7 @@ function Profile({ aws_id, user, goingTo, recommended, huddles }: Props) {
 
         {Array.isArray(huddlesUserIsGoing) && huddlesUserIsGoing.length ? (
           <>
-            <h1 className='pt-4 text-3xl font-yantra text-palette-dark font-medium'>
+            <h1 className="pt-4 text-3xl font-yantra text-palette-dark font-medium">
               Other Huddles that I&lsquo;m going to:
             </h1>
             <HuddleCarousel
@@ -222,7 +222,7 @@ function Profile({ aws_id, user, goingTo, recommended, huddles }: Props) {
 
         {Array.isArray(lastRow.huddles) && lastRow.huddles.length ? (
           <>
-            <h1 className='pt-4 text-3xl text-palette-dark font-medium'>
+            <h1 className="pt-4 text-3xl text-palette-dark font-medium">
               {lastRow.name} huddles:
             </h1>
             {huddlesUserIsGoing && (
@@ -236,7 +236,7 @@ function Profile({ aws_id, user, goingTo, recommended, huddles }: Props) {
             )}
           </>
         ) : (
-          <h1 className='pt-6 sm:py-6 p-4 text-3xl font-karla font-extralight'>
+          <h1 className="pt-6 sm:py-6 p-4 text-3xl font-karla font-extralight">
             {
               " We don't have huddles to recommend you yet. Try adding more interests to your profile."
             }
@@ -267,7 +267,7 @@ export const getServerSideProps = async ({ req, res }: Context) => {
     );
     const user: User[] = await getUserById(username);
     if (!user.length) {
-      res.writeHead(302, { Location: '/' });
+      res.writeHead(302, { Location: "/" });
       res.end();
       return {
         props: {},
@@ -283,11 +283,10 @@ export const getServerSideProps = async ({ req, res }: Context) => {
       },
     };
   } catch (err) {
-    res.writeHead(302, { Location: '/' });
+    res.writeHead(302, { Location: "/" });
     res.end();
     return {
       props: {},
     };
   }
 };
-
